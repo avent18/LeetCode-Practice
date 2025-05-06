cccf0b0b8f054fd337d1724c52efc123339f35f7
avent18 <naveenkumarnghspn@gmail.com>
2025-05-07 01:46:37 +0530

    new problem added

--- 790.Domino and Tromino Tiling/Solution.java
+++ 790.Domino and Tromino Tiling/Solution.java
@@ -9,11 +9,21 @@ class Solution {
         dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
+      
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = (2 * dp[i - 1] % MODULO + dp[i - 3]) % MODULO;
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MODULO;
+            
         }
 
         return (int) dp[n]; 
     }
 }
+
+// n = 0 1 way
+//n=1 1 way  2*1
+//n=2 2 way 2*2
+//n=3  5 ways = 2*2+1
+//n=4 11 ways = 2*5+1
+//n=5 23 ways = 2*11+2
+//a[n] = 2*a[n-1]+a[n-3]
